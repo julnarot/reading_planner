7c7e658ab9e52181ea9cc53a4ae73c1140d90fbc
@Julnarot <rauljhonatan.tola@gmail.com>
2023-11-28 23:57:41 -0500

    feat(frontend/menu): add menu component

---
+++ rp_webapp/src/app/core/menu/menu.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+interface Menu {
+  name: string;
+  icon: string;
+  link: string;
+}
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [CommonModule, MatListModule, MatIconModule, RouterLink],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss',
+})
+export class MenuComponent {
+  menuList: Menu[] = [
+    {
+      name: 'Read',
+      icon: 'folder',
+      link: '/read'
+    },
+    {
+      name: 'books',
+      icon: 'folder',
+      link: '/books'
+    },
+    {
+      name: 'Store',
+      icon: 'folder',
+      link: '/store'
+    },
+  ];
+}
