9d2a95ba1772e139151e3b1dcc924962ae8bde14
@Julnarot <rauljhonatan.tola@gmail.com>
2023-11-28 22:47:16 -0500

    conf(backend): backend api resource application was added

---
+++ rp_api/src/test/java/com/julnarot/rpapi/RpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julnarot.rpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
