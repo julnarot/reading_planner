0a2fa3925d3d7cd02723927debc26482bf91bedc
@Julnarot <rauljhonatan.tola@gmail.com>
2023-11-28 23:58:21 -0500

    feat(frontend/core): add components to router navigate

--- rp_webapp/src/app/app.component.ts
+++ rp_webapp/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MenuComponent } from './core/menu/menu.component';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FlexLayoutModule, MatSidenavModule, MenuComponent, DashboardComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'rp_webapp';
+  showFiller = true;
 }
--- rp_webapp/src/app/app.routes.ts
+++ rp_webapp/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { BooksComponent } from './components/books/books.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+
+    ],
+
+  },
+  {
+    path: 'books',
+    component: BooksComponent,
+    data: { title: 'Dashboard' },
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
